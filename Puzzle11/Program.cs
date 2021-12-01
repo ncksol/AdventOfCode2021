f269fc613db7a61da0188e8a68cd717c4b7462c7
Nick Sologoub <nick.sologoub@newday.co.uk>
2021-12-01 10:45:51 +0000

    Add project files.

---
+++ Puzzle11/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = new List<int>();
+
+var file = new FileInfo("TextFile1.txt");
+using (var textReader = new StreamReader(file.OpenRead()))
+{
+    while (textReader.EndOfStream == false)
+    {
+        var num = Convert.ToInt32(textReader.ReadLine());
+        input.Add(num);
+    }
+}
+
+var count = 0;
+
+for (int i = 0; i < input.Count; i++)
+{
+    if (i > 0 && input[i] > input[i - 1])
+        count++;
+}
+
+Console.WriteLine(count.ToString());
---
+++ Puzzle12/Program.cs
@@ -0,0 +1,29 @@
+﻿var input = new List<int>();
+
+var file = new FileInfo("TextFile1.txt");
+using (var textReader = new StreamReader(file.OpenRead()))
+{
+    while (textReader.EndOfStream == false)
+    {
+        var num = Convert.ToInt32(textReader.ReadLine());
+        input.Add(num);
+    }
+}
+
+//input = new List<int> { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+
+var count = 0;
+
+var i = 2;
+while (i + 1 < input.Count)
+{
+    var num1 = input[i - 2] + input[i - 1] + input[i];
+    var num2 = input[i - 1] + input[i] + input[i + 1];
+
+    if (num2 > num1)
+        count++;
+
+    i++;
+}
+
+Console.WriteLine(count.ToString());
