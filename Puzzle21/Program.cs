85a1f503f494fa9883bbcac3894c8566f4fc669b
Nick Sologoub <nick.sologoub@newday.co.uk>
2021-12-02 10:37:22 +0000

    puzzle 21

---
+++ Puzzle21/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = new List<KeyValuePair<string, int>>();
+
+var file = new FileInfo("TextFile1.txt");
+using (var textReader = new StreamReader(file.OpenRead()))
+{
+    while (textReader.EndOfStream == false)
+    {
+        var data = textReader.ReadLine().Split(' ');
+        input.Add(new KeyValuePair<string, int>(data[0], Convert.ToInt32(data[1])));
+    }
+}
+
+var hor = 0;
+var dp = 0;
+
+foreach (var move in input)
+{
+    switch (move.Key)
+    { 
+        case "forward":
+            hor += move.Value;
+            break;
+
+        case "down":
+            dp += move.Value;
+            break;
+
+        case "up":
+            dp -= move.Value;
+            break;
+
+    }
+}
+
+Console.WriteLine(hor * dp);
