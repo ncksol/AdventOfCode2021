030090e60dda3b3a69187dbcf9c0edd2557e76f9
Nick Sologoub <nick.sologoub@outlook.com>
2021-12-06 14:53:44 +0000

    puzzle 62

---
+++ Puzzle61/Program.cs
@@ -0,0 +1,38 @@
+﻿var input = new List<int>(new int[] { 3, 4, 3, 1, 2 });
+
+var file = new FileInfo("TextFile1.txt");
+using (var textReader = new StreamReader(file.OpenRead()))
+{
+    var data = textReader.ReadToEnd();
+    input = data.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+}
+
+var fishes = new long[9];
+
+foreach (var fish in input)
+{
+    fishes[fish]++;
+}
+
+
+for (int i = 0; i < 256; i++)
+{
+    var buffer = new long[9];
+    for (int a = 0; a < fishes.Length; a++)
+    {
+        if(a == 0)
+        {
+            buffer[6] += fishes[a];
+            buffer[8] += fishes[a];
+        }
+        else
+        {
+            buffer[a-1] += fishes[a];
+        }
+    }
+    fishes = buffer;
+}
+
+var sum = fishes.Sum();
+
+Console.WriteLine(sum);
